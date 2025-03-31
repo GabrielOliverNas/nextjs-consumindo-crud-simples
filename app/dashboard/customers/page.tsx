ec9569060d0afb75e504755a50062640645885f7
Gabriel de Oliveira <gabrielolivernas@gmail.com>
2025-03-31 11:53:40 -0300

    Implementacao inicial NextJS

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CustomersPage = () => {
+  return (
+    <div>
+      <h1>Customers</h1>
+      {/* Other page content */}
+    </div>
+  );
+};
+
+export default CustomersPage;
